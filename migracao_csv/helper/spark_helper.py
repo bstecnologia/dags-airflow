ee859e85db4ae4e611980c07d65e6116b02d8b25
JesseSBezerra <jessebezerra@hotmail.com.br>
2024-01-22 17:53:01 -0300

    .
    .

--- migracao_csv/helper/spark_helper.py
+++ migracao_csv/helper/spark_helper.py
@@ -10,7 +10,7 @@ def get_session():
              .appName("nova-data")
              .config("spark.sql.legacy.timeParserPolicy", "LEGACY")
              .config("spark.driver.host", "10.98.96.8")
-             .config("spark.driver.port", "43031")
+             #.config("spark.driver.port", "43031")
              .getOrCreate()
              )
     return spark
